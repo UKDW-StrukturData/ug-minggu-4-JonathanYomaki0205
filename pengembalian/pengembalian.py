45023a6b5ea7f529fb22917b06ef6c8a0e00ede4
JonathanYomaki71200573 <jonathan.andreas@ti.ukdw.ac.id>
2025-09-24 11:50:32 +0700

    update pengembalian.py dan app.py

--- pengembalian/pengembalian.py
+++ pengembalian/pengembalian.py
@@ -5,19 +5,23 @@ def process_pengembalian(buku, mahasiswa):
     
     def kembalikan_buku(nama, judul):
         # 1. Validasi mahasiswa
-        # TODO: melakukan pengecekan apakah nama mahasiswa ada dalam dictionary buku, kemudian
-        #    return (judul, "Gagal", "Mahasiswa tidak terdaftar")
+        if nama not in mahasiswa:
+            return (judul, "Gagal", "Mahasiswa tidak terdaftar")
 
         # 2. Validasi judul buku
-        # TODO: melakukan pengecekan apakah judul ada dalam dictionary buku, kemudian
-        #    return (judul, "Gagal", "Judul tidak ditemukan")
+        if judul not in buku:
+            return (judul, "Gagal", "Judul tidak ditemukan")
 
-        # 3. TODO: cek apakah buku tercatat sedang dipinjam oleh mahasiswa
-        # hint: gunakan if judul in mahasiswa[nama].get("pinjaman", [])
-        # jika iya: hapus dari daftar pinjaman
-        # lalu tambahkan stok buku +1
-        # jika status buku sebelumnya "Kosong", ubah menjadi "Tersedia"
-            # return (judul, "Berhasil", None)
+        # 3. Cek apakah buku tercatat sedang dipinjam oleh mahasiswa
+        if judul in mahasiswa[nama].get("pinjaman", []):
+            # Hapus dari daftar pinjaman
+            mahasiswa[nama]["pinjaman"].remove(judul)
+            # Tambahkan stok buku +1
+            buku[judul]["stok"] += 1
+            # Jika status buku sebelumnya "Kosong", ubah menjadi "Tersedia"
+            if buku[judul]["stok"] > 0 and buku[judul]["status"] == "Kosong":
+                buku[judul]["status"] = "Tersedia"
+            return (judul, "Berhasil", None)
 
         # 4. Jika tidak ada di daftar pinjaman
         return (judul, "Gagal", "Tidak tercatat sebagai pinjaman")
